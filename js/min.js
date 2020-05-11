e04e0af29f10de361c1222976b3c374da95ef853
Daniel Disowsa <42129480+DroidDaniel@users.noreply.github.com>
2020-05-10 23:34:37 -0700

    Add files via upload

--- js/min.js
+++ js/min.js
@@ -1,106 +1,32 @@
-$(document).ready(function() {
- $(".dropdown-toggle").click(function() {
-  $(".dropdown").slideToggle();
-  $(".dropdown-toggle img").toggleClass('rotated');
- });
+var a = 0;
+$(window).scroll(function() {
 
- $(".mb-menu").click(function() {
-  $(".side-nav").css('left', 0);
- });
+  var oTop = $('#counter').offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $('.counter-value').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
 
+        {
 
- $(".btn_cls").click(function() {
-  $(".side-nav").css('left', -300);
- });
+          duration: 2000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
 
-
- $(".accordion").click(function() {
-  $(".myBarinside1").animate({
-   width: '24%'
-  });
-
-  $(".myBarinside2").animate({
-   width: '58%'
-  });
-
-  $(".myBarinside3").animate({
-   width: '76%'
-  });
-
-
-  $(".myBarinside4").animate({
-   width: '17%'
-  });
-
-
-  $(".myBarinside5").animate({
-   width: '49%'
-  });
- });
+        });
+    });
+    a = 1;
+  }
 
 });
-
-
-
-
-$(document).ready(function() {
- $(".set > a").on("click", function() {
-  if ($(this).hasClass("active")) {
-   $(this).removeClass("active");
-   $(this)
-    .siblings(".content")
-    .slideUp(200);
-   $(".set > a i")
-    .removeClass("fa-minus")
-    .addClass("fa-plus");
-  } else {
-   $(".set > a i")
-    .removeClass("fa-minus")
-    .addClass("fa-plus");
-   $(this)
-    .find("i")
-    .removeClass("fa-plus")
-    .addClass("fa-minus");
-   $(".set > a").removeClass("active");
-   $(this).addClass("active");
-   $(".content").slideUp(200);
-   $(this)
-    .siblings(".content")
-    .slideDown(200);
-  }
- });
-});
-
-
-
-var i = 0;
-
-function move() {
- if (i == 0) {
-  i = 1;
-  var elem1 = document.getElementById("myBar1");
-  var elem2 = document.getElementById("myBar2");
-  var elem3 = document.getElementById("myBar3");
-  var elem4 = document.getElementById("myBar4");
-
-
-
-  var width = 1;
-  var id = setInterval(frame, 10);
-
-  function frame() {
-   if (width >= 76) {
-    clearInterval(id);
-    i = 0;
-   } else {
-    width++;
-    elem1.style.width = width + "%";
-    elem2.style.width = width + "%";
-    elem3.style.width = width + "%";
-    elem4.style.width = width + "%";
-
-
-   }
-  }
- }
-}
