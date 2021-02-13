b873c52075e02a2cc1b4382affe30d3e27c8ad9f
Daniel Disowsa <42129480+DroidDaniel@users.noreply.github.com>
2021-02-13 16:32:13 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,6 +6,25 @@ $(document).ready(function(){
         $(".nav__wrapper #menu-header-menu li a").toggleClass("nav-li--fade-in");
         $("body").toggleClass("no-scroll nav__wrapper__overlay");
     });
+    if ($(window).width() <= 1100) {
+        $(".nav__wrapper #menu-header-menu li a").click(function(){
+        $(".nav__wrapper #menu-header-menu li a").toggleClass("nav-li--fade-in");
+        $(".menu").removeClass("nav-width");
+        $(".navmenu-toggle").removeClass("open");
+        $("body").removeClass("no-scroll nav__wrapper__overlay");
+        });
+     }
+
+     $(window).resize(function(){
+        if ($(window).width() > 1100) {
+            $("body").removeClass("no-scroll nav__wrapper__overlay");
+        }else{
+            $(".navmenu-toggle").click(function(){
+                $("body").addClass("no-scroll nav__wrapper__overlay");
+            });
+        }
+        
+      });
     ////////////////Form input field label animation
     $('.form-input').focus(function(){
         $(this).parents('.form-group').addClass('focused');
@@ -20,22 +39,20 @@ $(document).ready(function(){
         } else {
           $(this).addClass('filled');
         }
-      })  
-
+      }) 
   });
 
 
-  document.getElementById("sub").addEventListener("click", validateEmail);
+  document.getElementById("sub").addEventListener("click", validateForm);
 
-  function validateEmail(){
-
+  function validateForm(){
     //Name field
     var nameField = document.getElementById("username");
     if ( nameField.value == "" ) {
         nameField.classList.add("form_error_class");
     }
     else{
-        console.log("success")
+        emailField.classList.remove("form_error_class");
     }
 
     //email field
@@ -45,7 +62,7 @@ $(document).ready(function(){
         emailField.classList.add("form_error_class");
     }
     else{
-        console.log("success")
+        emailField.classList.remove("form_error_class");
     }
 
     //message field
@@ -54,6 +71,29 @@ $(document).ready(function(){
         messageField.classList.add("form_error_class");
     }
     else{
-        console.log("success")
+        emailField.classList.remove("form_error_class");
     }
+}
+
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    //smoothscroll
+    $('#menu-center ul li').on('click', function () {
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop() + 100;
+    $('#menu-center ul li a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#menu-center ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
 }
